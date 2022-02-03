dd3a621cf44ed9e707c969ab4ecbca58ea0de444
Charles Clement <charles.clement@cpe.fr>
2022-02-03 11:55:29 +0100

    test_smarty2

--- php/index.php
+++ php/index.php
@@ -1,6 +1,8 @@
 <?php
 echo("test php");
-require_once('/libs/Smarty.class.php');
+require_once('../libs/Smarty.class.php');
+echo("test2");
 $smarty= new Smarty();
+echo("test3");
 $smarty->assign('var1','test_var');
 $smarty->display('../html/index.tpl');
