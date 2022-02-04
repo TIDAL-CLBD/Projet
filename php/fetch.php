106466984437c42dfac0a68c47fc8228d0f847ca
Timothee Levilly <tim.levilly@gmail.com>
2022-02-04 12:13:11 +0100

    fintp3

---
+++ php/fetch.php
@@ -0,0 +1,10 @@
+<?php
+$n=3;
+$val=0;
+//todo array (placeholder qui contiendra le res de la requete sql a terme)
+//todo recup infos du js pour faire la requete
+//todo securité vs injection sql
+//todo mise en page json
+
+//dans le js: interpréter le json + adapter le css et le html en fct
+echo $a;
