98998daa21318c9de5a6e45279199056260e9a5b
tidal <tim.levilly@gmail.com>
2022-02-04 09:52:02 +0100

    smarty fonctionne

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir smarty
+cd smarty
+mkdir templates
+mkdir templates_c
+mkdir configs
+mkdir cache
+chown :www-data -R .
+chmod 770 -R .
+cd ..
--- php/index.php
+++ php/index.php
@@ -1,8 +1,15 @@
 <?php
 echo("test php");
-require_once('../smarty/libs/Smarty.class.php');
+require_once('../libs/Smarty.class.php');
 echo("test2");
 $smarty= new Smarty();
 echo("test3");
 $smarty->assign('var1','test_var');
+$smarty->debugging = true;
+
+$smarty->setTemplateDir('../smarty/templates/');
+$smarty->setCompileDir('../smarty/templates_c/');
+$smarty->setConfigDir('../smarty/configs/');
+$smarty->setCacheDir('../smarty/cache/');
+
 $smarty->display('../html/index.tpl');
